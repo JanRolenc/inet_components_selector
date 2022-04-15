c58c92601b9e5a96f637cf5f4b61183ea57eba54
Jan Rolenc <jrolenc@centrum.cz>
2022-04-15 20:33:30 +0200

    styling of unit containers in menu and in input

--- src/PersonsSelect/OptionInput.tsx
+++ src/PersonsSelect/OptionInput.tsx
@@ -1,32 +1,34 @@
-import React from "react";
-import { IUnit } from "./interfaces/interfaces";
+import React from 'react'
+import { IUnit } from './interfaces/interfaces'
 
 interface Props {
-  unit: IUnit;
-  onClickSelectUnit(unitNameToSelect: any): void;
+  unit: IUnit
+  onClickSelectUnit(unitNameToSelect: any): void
 }
 
 const OptionInput = ({ unit, onClickSelectUnit }: Props) => {
   return (
-    <span
+    <div
       className="unit-container-input"
       onClick={() => onClickSelectUnit(unit.id)}
       title={unit.nameCs}
     >
       <span
         style={{
-          height: "10px",
-          width: "10px",
-          borderRadius: "50%",
-          display: "inline-block",
+          height: '10px',
+          width: '10px',
+          borderRadius: '50%',
+          display: 'inline-block',
           backgroundColor: unit.color,
         }}
       ></span>
-      &#160;
-      {unit.shortCs}
-      &#160;&#160;
-      <span className="clear-indicator-unit">x</span>
-    </span>
-  );
-};
-export default OptionInput;
+      {/* &#160; */}
+      <span style={{ padding: '5px 12px 5px 6px' }}>{unit.shortCs}</span>
+      {/* &#160;&#160; */}
+      <div className="clear-indicator-unit-container">
+        <span className="clear-indicator-unit">x</span>
+      </div>
+    </div>
+  )
+}
+export default OptionInput
